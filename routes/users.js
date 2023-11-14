dede37e6d1c6e1f15623f631343e9e49d1d57fa4
veetiengblom <Veeti_engblom@hotmail.com>
2023-11-14 21:06:33 +0200

    fixed user

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,15 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get("/user/:id", function (req, res, next) {
+  const { id } = req.params;
+  const existingUser = todoList.find((user) => user.name === id);
+  if (existingUser) {
+    res.send(existingUser.todos);
+  } else {
+    const resText = { text: ["User not found"] };
+    res.send(resText.text);
+  }
+});
+
 module.exports = router;
